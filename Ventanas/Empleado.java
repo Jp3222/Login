9b4d85c626cb194d53f9c0e0d7e8a6b1839927b6
Juan Pablo Campos <64780248+Jp3222@users.noreply.github.com>
2021-03-02 23:34:54 -0600

    Create Empleado.java

---
+++ Ventanas/Empleado.java
@@ -0,0 +1,55 @@
+package Ventanas;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+import login.Imagen;
+
+public class Empleado extends JFrame {
+
+    private JLabel jlbTexto1, jlbTexto2;
+    private JTextField jtfUsuario;
+    private JPasswordField jpfContraseña;
+    private JButton jbtIngresar;
+    private JPanel PanelPrincipal, PanelBarra;
+    private Imagen im;
+
+    private String user, password;
+
+    public Empleado() {
+        setTitle("Ventana de empleados");
+        setMinimumSize(new Dimension(600, 600));
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        Panel();
+    }
+
+    private void Panel() {
+        PanelPrincipal = new JPanel(null);
+        PanelPrincipal.setBackground(Color.lightGray);
+        PanelPrincipal.add(Barra());
+        setContentPane(PanelPrincipal);
+    }
+
+    private JPanel Barra() {
+        PanelBarra = new JPanel(null);
+        PanelBarra.setBackground(Color.black);
+        PanelBarra.setSize(PanelPrincipal.getMaximumSize().width, 50);
+        PanelBarra.setLocation(0, 0);
+        JLabel texto1 = new JLabel("Empleado");
+        texto1.setSize(200, 30);
+        texto1.setLocation(150, 5);
+        texto1.setForeground(Color.white);
+        texto1.setFont(new Font("Arial", 2, 30));
+        PanelBarra.add(texto1);
+        return PanelBarra;
+    }
+}
